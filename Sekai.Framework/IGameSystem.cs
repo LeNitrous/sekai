8b672e81856275d34d679ee8f283ad43d9aff887
Ayane Satomi <ayane@vignetteapp.org>
2022-06-07 20:37:57 +0800

    Remove this comment
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

--- Sekai.Framework/IGameSystem.cs
+++ Sekai.Framework/IGameSystem.cs
@@ -3,9 +3,7 @@
 
 namespace Sekai.Framework;
 
-/// <summary>
-/// The base class for all game systems.
-/// </summary>
+
 public interface IGameSystem : IFrameworkComponent
 {
     // FIXME: This is empty by default since GameSystems are pretty much varied with their purpose.
