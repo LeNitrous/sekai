c9062c7e2de27582c9669b557c7203b3e3c52334
Ayane Satomi <ayane@vignetteapp.org>
2022-06-07 20:38:55 +0800

    These annotations is going to fuck me up I swear
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

--- Sekai.Framework/IFrameworkComponent.cs
+++ Sekai.Framework/IFrameworkComponent.cs
@@ -1,9 +1,9 @@
 // Copyright (c) The Vignette Authors
 // Licensed under MIT. See LICENSE for details.
-
 using System;
 
 namespace Sekai.Framework;
+
 public interface IFrameworkComponent : IDisposable
 {
     protected virtual void Destroy() { }
