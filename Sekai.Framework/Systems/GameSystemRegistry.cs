3e9e3472781296a7c84deed22bd4d233dd69934d
Nitrous <nathan.alo2000@gmail.com>
2022-06-12 17:53:17 +0800

    implement relationship between loadables

--- Sekai.Framework/Systems/GameSystemRegistry.cs
+++ Sekai.Framework/Systems/GameSystemRegistry.cs
@@ -24,7 +24,8 @@ public class GameSystemRegistry : FrameworkObject, IEnumerable<GameSystem>
     /// <summary>
     /// Gets a registered game system.
     /// </summary>
-    public T GetSystem<T>() where T : GameSystem
+    public T GetSystem<T>()
+        where T : GameSystem, new()
     {
         return !systems.TryGetValue(typeof(T), out var gs)
             ? throw new InvalidCastException($"No such system of type {typeof(T).Name} is found.")
@@ -34,35 +35,42 @@ public class GameSystemRegistry : FrameworkObject, IEnumerable<GameSystem>
     /// <summary>
     /// Registers a game system.
     /// </summary>
-    public void Register<T>() where T : GameSystem, new()
+    public void Register<T>()
+        where T : GameSystem, new()
     {
         if (systems.ContainsKey(typeof(T)))
             throw new InvalidOperationException($"System of type {typeof(T).Name} is already registered.");
 
         var system = Activator.CreateInstance<T>();
-        system.Services.Parent = game.Services;
+        ((ILoadableObjectContainer)game).Add(system);
         systems.Add(typeof(T), system);
-        system.Initialize();
     }
 
     /// <summary>
     /// Unregisters a game system.
     /// </summary>
-    public void Unregister<T>() where T : GameSystem
+    public void Unregister<T>()
+        where T : GameSystem, new()
     {
         if (!systems.ContainsKey(typeof(T)))
             throw new InvalidOperationException($"System of type {typeof(T).Name} is not registered.");
 
+        var system = systems[typeof(T)];
+
+        ((ILoadableObjectContainer)game).Remove(system);
         systems.Remove(typeof(T));
-        systems[typeof(T)].Dispose();
+        system.Dispose();
     }
 
     protected override void Destroy()
     {
         // we can't leave any game systems dangling
         // so we'll have to dispose them immediately on dispose.
-        foreach (var item in systems.Values)
-            item.Dispose();
+        foreach (var system in systems.Values)
+        {
+            ((ILoadableObjectContainer)game).Remove(system);
+            system.Dispose();
+        }
 
         systems.Clear();
     }
