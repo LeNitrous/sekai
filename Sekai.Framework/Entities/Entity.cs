7afcf39207440616a1881abc9ea75a6a039e288c
Ayane Satomi <ayane@vignetteapp.org>
2022-06-13 09:01:24 +0800

    Commit current work tree before I nuke this to oblivion
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

---
+++ Sekai.Framework/Entities/Entity.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+using System.Collections.Generic;
+
+namespace Sekai.Framework.Entities;
+
+public class Entity : FrameworkObject, ILoadable, IUpdateable
+{
+    public LoadableObject Parent { get; private set; }
+
+    public IReadOnlyList<LoadableObject> Children { get; private set; }
+
+    public void Update(double delta)
+    {
+
+    }
+
+    public void Add(LoadableObject lo)
+    {
+
+    }
+
+    public void AddRange(IEnumerable<LoadableObject> loc)
+    {
+
+    }
+
+    public void Remove(LoadableObject lo)
+    {
+
+    }
+
+    public void RemoveRange(IEnumerable<LoadableObject> loc)
+    {
+
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public void Destroy()
+    {
+        Clear();
+    }
+}
