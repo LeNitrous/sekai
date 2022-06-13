55e7c028901b02ea588ef39fea800e24a5c34e7f
Ayane Satomi <ayane@vignetteapp.org>
2022-06-13 10:36:49 +0800

    Implement basic data structures for Input System
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

---
+++ Sekai.Framework/IO/HID/ButtonCommand.cs
@@ -0,0 +1,25 @@
+// Copyright (c) The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+namespace Sekai.Framework.IO.HID;
+/// <summary>
+/// A struct defining a Key button command. 
+/// </summary>
+public struct ButtonCommand
+{
+    /// <summary>
+    /// The name of the Key button command.
+    /// Ex: IN_STRAFE.
+    /// </summary>
+    public string CmdName;
+
+    /// <summary>
+    /// The keys holding it down.
+    /// </summary>
+    public int[]? KeysDown;
+
+    /// <summary>
+    /// The current state of the key button command.
+    /// </summary>
+    public bool Active;
+}
---
+++ Sekai.Framework/IO/HID/InputSystem.cs
@@ -0,0 +1,11 @@
+// Copyright (c) The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+using Sekai.Framework.Systems;
+
+namespace Sekai.Framework.IO.HID;
+
+public class InputSystem : GameSystem
+{
+
+}
