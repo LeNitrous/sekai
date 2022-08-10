3d396da7a0ee82fcb53e739f8c9c20bbc21649e0
Ayane Satomi <ayane@vignetteapp.org>
2022-08-10 18:48:01 +0800

    Port SDL2 implementation from MonoGame
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

---
+++ source/framework/Sekai.Framework/Interop/CurrentPlatform.cs
@@ -0,0 +1,110 @@
+// Copyright (c) The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+// Contains code from MonoGame.
+// Copyright 2021 the MonoGame Team.
+// Licensed under the Microsoft Public License and MIT.
+// See https://github.com/MonoGame/MonoGame/blob/develop/LICENSE.txt.
+using System;
+using System.Runtime.InteropServices;
+
+namespace Sekai.Framework.Interop;
+public enum OS
+{
+    Windows,
+    Linux,
+    MacOSX,
+    Unknown
+}
+
+public static class CurrentPlatform
+{
+#pragma warning disable IDE1006 // Naming Styles
+    private static bool _init = false;
+#pragma warning restore IDE1006 // Naming Styles
+    private static OS os;
+
+    [DllImport("libc")]
+    private static extern int uname(IntPtr buf);
+
+    private static void init()
+    {
+        if (_init)
+            return;
+
+        var pid = Environment.OSVersion.Platform;
+
+        switch (pid)
+        {
+            case PlatformID.Win32NT:
+            case PlatformID.Win32S:
+            case PlatformID.Win32Windows:
+            case PlatformID.WinCE:
+                os = OS.Windows;
+                break;
+            case PlatformID.MacOSX:
+                os = OS.MacOSX;
+                break;
+            case PlatformID.Unix:
+                os = OS.MacOSX;
+
+                var buf = IntPtr.Zero;
+
+                try
+                {
+                    buf = Marshal.AllocHGlobal(8192);
+
+                    if (uname(buf) == 0 && Marshal.PtrToStringAnsi(buf) == "Linux")
+                        os = OS.Linux;
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    if (buf != IntPtr.Zero)
+                        Marshal.FreeHGlobal(buf);
+                }
+
+                break;
+            // we don't support the following so we'll just mark them unknown
+            case PlatformID.Xbox:
+                os = OS.Unknown;
+                break;
+            case PlatformID.Other:
+                os = OS.Unknown;
+                break;
+            default:
+                os = OS.Unknown;
+                break;
+        }
+
+        _init = true;
+    }
+
+    public static OS OS
+    {
+        get
+        {
+            init();
+            return os;
+        }
+    }
+
+    public static string Rid
+    {
+        get
+        {
+            if (OS == OS.Windows && Environment.Is64BitProcess)
+                return "win-x64";
+            else if (OS == OS.Windows && !Environment.Is64BitProcess)
+                return "win-x86";
+            else if (OS == OS.Linux)
+                return "linux-x64";
+            else if (OS == OS.MacOSX)
+                return "osx";
+            else
+                return "unknown";
+        }
+    }
+}
---
+++ source/framework/Sekai.Framework/Interop/FuncLoader.Desktop.cs
@@ -0,0 +1,128 @@
+// Copyright (c) The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+// Contains code from MonoGame.
+// Copyright 2021 the MonoGame Team.
+// Licensed under the Microsoft Public License and MIT.
+// See https://github.com/MonoGame/MonoGame/blob/develop/LICENSE.txt.
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Sekai.Framework.Interop;
+public class FuncLoader
+{
+    private class Windows
+    {
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr LoadLibraryW(string lpszLib);
+    }
+
+    private class Linux
+    {
+        [DllImport("libdl.so.2")]
+        public static extern IntPtr dlopen(string path, int flags);
+
+        [DllImport("libdl.so.2")]
+        public static extern IntPtr dlsym(IntPtr handle, string symbol);
+    }
+
+    private class OSX
+    {
+        [DllImport("/usr/lib/libSystem.dylib")]
+        public static extern IntPtr dlopen(string path, int flags);
+
+        [DllImport("/usr/lib/libSystem.dylib")]
+        public static extern IntPtr dlsym(IntPtr handle, string symbol);
+    }
+
+    private const int rtld_lazy = 0x0001;
+
+    public static IntPtr LoadLibraryExt(string libname)
+    {
+        var ret = IntPtr.Zero;
+        string? assemblyLocation = Path.GetDirectoryName(typeof(FuncLoader).Assembly.Location) ?? "./";
+
+        // Try .NET Framework / mono locations
+        if (CurrentPlatform.OS == OS.MacOSX)
+        {
+            ret = LoadLibrary(Path.Combine(assemblyLocation, libname));
+
+            // Look in Frameworks for .app bundles
+            if (ret == IntPtr.Zero)
+                ret = LoadLibrary(Path.Combine(assemblyLocation, "..", "Frameworks", libname));
+        }
+        else
+        {
+            if (Environment.Is64BitProcess)
+                ret = LoadLibrary(Path.Combine(assemblyLocation, "x64", libname));
+            else
+                ret = LoadLibrary(Path.Combine(assemblyLocation, "x86", libname));
+        }
+
+        // Try .NET Core development locations
+        if (ret == IntPtr.Zero)
+            ret = LoadLibrary(Path.Combine(assemblyLocation, "runtimes", CurrentPlatform.Rid, "native", libname));
+
+        // Try current folder (.NET Core will copy it there after publish)
+        if (ret == IntPtr.Zero)
+            ret = LoadLibrary(Path.Combine(assemblyLocation, libname));
+
+        // Try alternate way of checking current folder
+        // assemblyLocation is null if we are inside macOS app bundle
+        if (ret == IntPtr.Zero)
+            ret = LoadLibrary(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, libname));
+
+        // Try loading system library
+        if (ret == IntPtr.Zero)
+            ret = LoadLibrary(libname);
+
+        // Welp, all failed, PANIC!!!
+        if (ret == IntPtr.Zero)
+            throw new Exception("Failed to load library: " + libname);
+
+        return ret;
+    }
+
+    public static IntPtr LoadLibrary(string libname)
+    {
+        if (CurrentPlatform.OS == OS.Windows)
+            return Windows.LoadLibraryW(libname);
+
+        if (CurrentPlatform.OS == OS.MacOSX)
+            return OSX.dlopen(libname, rtld_lazy);
+
+        return Linux.dlopen(libname, rtld_lazy);
+    }
+
+    public static T LoadFunction<T>(IntPtr library, string function, bool throwIfNotFound = false)
+    {
+        var ret = IntPtr.Zero;
+
+        if (CurrentPlatform.OS == OS.Windows)
+            ret = Windows.GetProcAddress(library, function);
+        else if (CurrentPlatform.OS == OS.MacOSX)
+            ret = OSX.dlsym(library, function);
+        else
+            ret = Linux.dlsym(library, function);
+
+        if (ret == IntPtr.Zero)
+        {
+            if (throwIfNotFound)
+                throw new EntryPointNotFoundException(function);
+
+#pragma warning disable CS8603 // Possible null reference return.
+            return default;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+#if NETSTANDARD
+            return Marshal.GetDelegateForFunctionPointer<T>(ret);
+#else
+        return (T)(object)Marshal.GetDelegateForFunctionPointer(ret, typeof(T));
+#endif
+    }
+}
