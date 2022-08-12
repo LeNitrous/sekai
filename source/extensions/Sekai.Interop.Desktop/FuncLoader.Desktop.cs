c4b8566cef9c07fddb9ad3ae27345a399a8653fd
Ayane Satomi <ayane@vignetteapp.org>
2022-08-12 18:20:37 +0800

    Rewrite library and provide documentation
    Original implementation didn't have documentation so I provided one
    
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

--- source/extensions/Sekai.Interop.Desktop/FuncLoader.Desktop.cs
+++ source/extensions/Sekai.Interop.Desktop/FuncLoader.Desktop.cs
@@ -10,63 +10,91 @@ using System.Runtime.InteropServices;
 namespace Sekai.Interop.Desktop;
 public class FuncLoader
 {
-    public static IntPtr LoadLibraryExt(string libname)
+    /// <summary>
+    /// Loads the library from a given name.
+    /// Depending on the runtime, it will try to load from the current working directory, then to the system.
+    /// On macOS, it will try to load from the the .NET Framework/Mono locations first then the system.
+    /// </summary>
+    /// <param name="name">The name of the library to load.</param>
+    /// <returns>The handle to the library.</returns>
+    public static IntPtr LoadLibraryExt(string libName)
     {
         var ret = IntPtr.Zero;
         string? assemblyLocation = Path.GetDirectoryName(typeof(FuncLoader).Assembly.Location) ?? Environment.CurrentDirectory;
 
-        // Try .NET Framework / mono locations
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            ret = NativeLibrary.Load(Path.Combine(assemblyLocation, libname));
-
-            // Look in Frameworks for .app bundles
-            if (ret == IntPtr.Zero)
-                ret = NativeLibrary.Load(Path.Combine(assemblyLocation, "..", "Frameworks", libname));
+            // let's check if we can load it from the .NET Framework or the Mono Locations
+            if (NativeLibrary.TryLoad(Path.Combine(assemblyLocation, libName), out var handle))
+              ret = handle;
+            // no? let's try the Framework .app bundle locations.
+            else if (NativeLibrary.TryLoad(Path.Combine(assemblyLocation, "..", "Frameworks", libName), out var syshandle))
+              ret = syshandle;
+            else
+              ret = IntPtr.Zero;
         }
         else
         {
             if (Environment.Is64BitProcess)
-                ret = NativeLibrary.Load(Path.Combine(assemblyLocation, "x64", libname));
+               ret = NativeLibrary.Load(Path.Combine(assemblyLocation, "x64", libName));
             else
-                ret = NativeLibrary.Load(Path.Combine(assemblyLocation, "x86", libname));
+              ret = NativeLibrary.Load(Path.Combine(assemblyLocation, "x86", libName));
         }
 
         // Try .NET Core development locations
         if (ret == IntPtr.Zero)
-            ret = NativeLibrary.Load(Path.Combine(assemblyLocation, "runtimes", RuntimeInformation.RuntimeIdentifier, "native", libname));
+        {
+          if (NativeLibrary.TryLoad(Path.Combine(assemblyLocation, "runtimes", RuntimeInformation.RuntimeIdentifier, "native", libName), out var handle))
+            ret = handle;
+          else
+            ret = IntPtr.Zero;
+        }
 
-        // Try current folder (.NET Core will copy it there after publish)
+        // Try current folder (usually this is the production build from `dotnet publish` goes to.)
         if (ret == IntPtr.Zero)
-            ret = NativeLibrary.Load(Path.Combine(assemblyLocation, libname));
+        {
+          if (NativeLibrary.TryLoad(Path.Combine(Environment.CurrentDirectory, libName), out var handle))
+            ret = handle;
+          else
+            ret = IntPtr.Zero;
+        }
 
-        // Try alternate way of checking current folder
-        // assemblyLocation is null if we are inside macOS app bundle
+        // last-ditch effort: load from the system library and pray it works
         if (ret == IntPtr.Zero)
-            ret = NativeLibrary.Load(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, libname));
+        {
+          if (NativeLibrary.TryLoad(libName, out var handle))
+            ret = handle;
+          else
+            ret = IntPtr.Zero;
+        }
 
-        // Try loading system library
+        // nothing works? too bad, let's tell everyone we can't load it.
         if (ret == IntPtr.Zero)
-            ret = NativeLibrary.Load(libname);
+          throw new Exception($"Could not load library {libName}");
 
-        // Welp, all failed, PANIC!!!
-        if (ret == IntPtr.Zero)
-            throw new Exception("Failed to load library: " + libname);
 
         return ret;
     }
 
+    /// <summary>
+    /// Loads the function provided the handle to the library is supplied.
+    /// </summary>
+    /// <param name="handle">The handle to the library.</param>
+    /// <param name="function">The name of the function to load.</param>
+    /// <returns>The function pointer.</returns>
     public static T LoadFunction<T>(IntPtr handle, string function)
     {
-        try
-        {
-            // let's discern symbols from a generic, then return a Delegate out of that.
-            return (T)(object)Marshal.GetDelegateForFunctionPointer(NativeLibrary.GetExport(handle, function), typeof(T));
+        var ret = IntPtr.Zero;
 
-        }
-        catch (EntryPointNotFoundException e)
+        if (NativeLibrary.TryGetExport(handle, function, out var funcHandle))
         {
-            throw new EntryPointNotFoundException("Could not find function, check if the defined symbol is exported in the library.", e);
+            ret = funcHandle;
         }
+        else
+        {
+            throw new Exception($"Could not find function {function}");
+        }
+
+        return (T)(object)Marshal.GetDelegateForFunctionPointer(ret, typeof(T));
     }
 }
