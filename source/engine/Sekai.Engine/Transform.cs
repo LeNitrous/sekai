c4392ed221ddc734604de854e022309853d4801c
Ayane Satomi <ayane@vignetteapp.org>
2022-08-20 09:21:36 +0800

    Document the datastructures
    I might get lost
    
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

--- source/engine/Sekai.Engine/Transform.cs
+++ source/engine/Sekai.Engine/Transform.cs
@@ -4,11 +4,22 @@
 using System.Numerics;
 
 namespace Sekai.Engine;
+
+/// <summary>
+/// Represents a Transform for a Entity.
+/// </summary>
 public class Transform : Component
 {
+    /// <summary>
+    /// Position of the Entity.
+    /// </summary>
     public Vector3 Position { get; set; }
-
+    /// <summary>
+    /// Rotation of the Entity.
+    /// </summary>
     public Quaternion Rotation { get; set; }
-
+    /// <summary>
+    /// Scale of the Entity.
+    /// </summary>
     public Vector3 Scale { get; set; }
 }
