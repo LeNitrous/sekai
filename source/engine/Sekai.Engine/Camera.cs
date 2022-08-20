e19aa4770e97d9842a50ac85c84f7a31f92dfcb1
Ayane Satomi <ayane@vignetteapp.org>
2022-08-20 09:21:36 +0800

    Document the datastructures
    I might get lost
    
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

--- source/engine/Sekai.Engine/Camera.cs
+++ source/engine/Sekai.Engine/Camera.cs
@@ -2,6 +2,10 @@
 // Licensed under MIT. See LICENSE for details.
 
 namespace Sekai.Engine;
+
+/// <summary>
+/// Represent a camera in the scene.
+/// </summary>
 public class Camera : Component
 {
     private struct CameraInset
@@ -12,9 +16,18 @@ public class Camera : Component
         internal float Bottom;
     }
 
+    /// <summary>
+    /// Z-position of the 3D Camera projections. Affects FOV greatly.
+    /// </summary>
     public float PositionZ3D = 2000f;
 
+    /// <summary>
+    /// Near Clip Plane of the 3D Camera.
+    /// </summary>
     public float NearClipPlane = 0.0001f;
 
+    /// <summary>
+    /// Far Clip Plane of the 3D camera projection.
+    /// </summary>
     public float FarClipPlane = 5000f;
 }
