33abe7205050a5090006b0ebfa69ec1ae5114f5c
Ayane <ayane@vignetteapp.org>
2022-11-07 14:50:06 +0800

    Add more helper methods for later
    Signed-off-by: Ayane <ayane@vignetteapp.org>

--- tests/Sekai.Experimental.Mathematics.Tests/TestUtils.cs
+++ tests/Sekai.Experimental.Mathematics.Tests/TestUtils.cs
@@ -13,4 +13,24 @@ internal static class TestUtils
     {
         return new Vector4(val.X, val.Y, val.Z, val.W);
     }
+
+    public static System.Numerics.Vector3 ConvertToSystemVec3(Vector3 val)
+    {
+        return new System.Numerics.Vector3(val.X, val.Y, val.Z);
+    }
+
+    public static Vector3 ConvertFromSystemVec3(System.Numerics.Vector3 val)
+    {
+        return new Vector3(val.X, val.Y, val.Z);
+    }
+
+    public static System.Numerics.Vector2 ConvertToSystemVec2(Vector2 val)
+    {
+        return new System.Numerics.Vector2(val.X, val.Y);
+    }
+
+    public static Vector2 ConvertFromSystemVec2(System.Numerics.Vector2 val)
+    {
+        return new Vector2(val.X, val.Y);
+    }
 }
