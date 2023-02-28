2b5678141cc9c68f8b382180b4212beaa7efb0f1
Nitrous <nathan.alo2000@gmail.com>
2023-02-28 21:50:20 +0800

    add `IReferenceable`

---
+++ source/Sekai.Core/Serialization/IReferenceable.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Cosyne and The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+using System;
+
+namespace Sekai.Serialization;
+
+/// <summary>
+/// An interface denoting an object can be referenced.
+/// </summary>
+public interface IReferenceable
+{
+    /// <summary>
+    /// Gets the referenceable's identifier.
+    /// </summary>
+    Guid Id { get; }
+}
