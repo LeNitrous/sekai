514cfd8234b2274f7cd310119a45eb3141f23709
Nitrous <nathan.alo2000@gmail.com>
2023-03-01 19:59:14 +0800

    refactor node collection tests

--- tests/Sekai.Core.Tests/Scenes/Node_CollectionTests.cs
+++ tests/Sekai.Core.Tests/Scenes/Node_CollectionTests.cs
@@ -11,55 +11,16 @@ public class Node_CollectionTests
     [Test]
     public void Node_ShouldAddOther()
     {
-        var a = new Node();
-        var b = new Node();
-
-        Assert.Multiple(() =>
-        {
-            Assert.That(() => a.Add(b), Throws.Nothing);
-            Assert.That(a, Does.Contain(b));
-            Assert.That(b.Parent, Is.EqualTo(a));
-        });
-    }
-
-    [Test]
-    public void Node_ShouldAddRange()
-    {
         var node = new Node();
 
-        Assert.Multiple(() =>
-        {
-            Assert.That(node, Has.Count.Zero);
-            Assert.That(node.Children, Is.Empty);
-            Assert.That(() => node.Children = new[] { new Node(), new Node(), new Node() }, Throws.Nothing);
-            Assert.That(node, Has.Count.Not.Zero);
-            Assert.That(node.Children, Is.Not.Empty);
-        });
-    }
-
-    [Test]
-    public void Node_ShouldClearChildren()
-    {
-        var node = new Node();
-
-        Assert.Multiple(() =>
-        {
-            Assert.That(node.Clear, Throws.Nothing);
-            Assert.That(() => node.Add(new Node()), Throws.Nothing);
-            Assert.That(node, Is.Not.Empty);
-        });
-
-        Assert.Multiple(() =>
-        {
-            Assert.That(node.Clear, Throws.Nothing);
-            Assert.That(node, Is.Empty);
-        });
+        Assert.That(() => node.Add(new Node()), Throws.Nothing);
     }
 
     [Test]
     public void Node_ShouldNotAddSelf()
     {
         var node = new Node();
+
         Assert.That(() => node.Add(node), Throws.InvalidOperationException);
     }
 
@@ -68,28 +29,124 @@ public class Node_CollectionTests
     {
         var a = new Node();
         var b = new Node();
+        a.Add(b);
 
-        Assert.That(() => a.Add(b), Throws.Nothing);
         Assert.That(() => a.Add(b), Throws.InvalidOperationException);
-
-        var c = new Node();
-        Assert.That(() => c.Add(b), Throws.InvalidOperationException);
     }
 
     [Test]
-    public void Node_ShouldRemoveOther()
+    public void Node_ShouldNotAddChildOfOther()
+    {
+        var a = new Node();
+        var b = new Node();
+        var c = new Node();
+        b.Add(c);
+
+        Assert.That(() => a.Add(c), Throws.InvalidOperationException);
+    }
+
+    [Test]
+    public void Node_ShouldAddRange()
+    {
+        var node = new Node();
+
+        Assert.That(() => node.AddRange(new[] { new Node() }), Throws.Nothing);
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Node_ShouldClearChildren(bool hasChild)
+    {
+        var node = new Node();
+
+        if (hasChild)
+        {
+            node.Add(new Node());
+        }
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(node.Clear, Throws.Nothing);
+            Assert.That(node, Is.Empty);
+            Assert.That(node.Clear, Throws.Nothing);
+        });
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Node_ShouldContainChild(bool hasChild)
     {
         var a = new Node();
         var b = new Node();
 
+        if (hasChild)
+        {
+            a.Add(b);
+        }
+
+        Assert.That(() => a.Contains(b), hasChild ? Is.True : Is.False);
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Node_ShouldRemoveChild(bool hasChild)
+    {
+        var a = new Node();
+        var b = new Node();
+
+        if (hasChild)
+        {
+            a.Add(b);
+        }
+
+        Assert.That(() => a.Remove(b), hasChild ? Is.True : Is.False);
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Node_ShouldGetChildren(bool hasChild)
+    {
+        var node = new Node();
+
+        if (hasChild)
+        {
+            node.Add(new Node());
+        }
+
+        Assert.That(node.Children, hasChild ? Is.Not.Empty : Is.Empty);
+    }
+
+    [Test]
+    public void Node_ShouldSetChildren()
+    {
+        var node = new Node();
+
         Assert.Multiple(() =>
         {
-            Assert.That(() => a.Remove(b), Is.False);
-            Assert.That(() => a.Add(b), Throws.Nothing);
-            Assert.That(() => a.Remove(b), Is.True);
-            Assert.That(b.Parent, Is.Null);
+            Assert.That(() => node.Children = new[] { new Node() }, Throws.Nothing);
+            Assert.That(node, Is.Not.Empty);
         });
+    }
 
-        Assert.That(() => a.Remove(b), Is.False);
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Node_ShouldHaveValidParent(bool hasParent)
+    {
+        var a = new Node();
+        var b = new Node();
+
+        if (hasParent)
+        {
+            a.Add(b);
+        }
+
+        Assert.That(b.Parent, hasParent ? Is.SameAs(a) : Is.Null);
+
+        if (hasParent)
+        {
+            a.Remove(b);
+
+            Assert.That(b.Parent, Is.Null);
+        }
     }
 }
