ff7cffdfd8d134186b6064103da6cbeb0cd8d7dd
Nitrous <nathan.alo2000@gmail.com>
2023-03-06 16:57:53 +0800

    add tick mode

---
+++ source/Sekai.Core/TickMode.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Cosyne and The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+namespace Sekai;
+
+/// <summary>
+/// An enumeration of game loop processing modes.
+/// </summary>
+public enum TickMode
+{
+    /// <summary>
+    /// Fixed time step.
+    /// </summary>
+    /// <remarks>
+    /// A tick occurs at a fixed time interval.
+    /// </remarks>
+    Fixed,
+
+    /// <summary>
+    /// Variable time step.
+    /// </summary>
+    /// <remarks>
+    /// A tick occurs at a variable time interval.
+    /// </remarks>
+    Variable,
+}
