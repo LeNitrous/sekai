288155537aff8256285b1a971e939ed0a2bbcd92
Nitrous <nathan.alo2000@gmail.com>
2023-03-22 08:39:09 +0800

    expose internals to test project

---
+++ source/Sekai.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Cosyne and The Vignette Authors
+// Licensed under MIT. See LICENSE for details.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Sekai.Core.Tests")]
