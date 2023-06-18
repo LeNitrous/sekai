674b291770122551f1aa86ca4695066299ad7d45
Nathan Alo <nathan.alo2000@gmail.com>
2023-06-18 09:40:20 +0000

    refactor LogWriterStream and make LogWriterText

--- source/Sekai.Framework/Logging/LogWriterStream.cs
+++ source/Sekai.Framework/Logging/LogWriterStream.cs
@@ -2,22 +2,20 @@
 // Licensed under MIT. See LICENSE for details.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace Sekai.Framework.Logging;
 
 /// <summary>
 /// A <see cref="LogWriter"/> that uses a <see cref="Stream"/>.
 /// </summary>
-public class LogWriterStream : LogWriter, IDisposable
+public abstract class LogWriterStream : LogWriter, IDisposable
 {
     private bool isDisposed;
+    private readonly bool leaveOpen;
     private readonly Stream stream;
-    private readonly Encoding encoding;
 
-    public LogWriterStream(Stream stream, Encoding? encoding = null)
+    public LogWriterStream(Stream stream, bool leaveOpen = false)
     {
         if (!stream.CanWrite)
         {
@@ -25,74 +23,7 @@ public class LogWriterStream : LogWriter, IDisposable
         }
 
         this.stream = stream;
-        this.encoding = encoding ?? Encoding.UTF8;
-    }
-
-    public sealed override void Flush()
-    {
-        if (isDisposed)
-        {
-            return;
-        }
-
-        stream.Flush();
-    }
-
-    public sealed override void Write(LogMessage message)
-    {
-        if (isDisposed)
-        {
-            return;
-        }
-
-        string formatted = Format(message);
-        Span<byte> buffer = stackalloc byte[encoding.GetByteCount(formatted)];
-
-        if (encoding.GetBytes(formatted.AsSpan(), buffer) > 0)
-        {
-            stream.Write(buffer);
-        }
-    }
-
-    /// <summary>
-    /// Formats a <see cref="LogMessage"/> as a <see cref="string"/>.
-    /// </summary>
-    /// <param name="message">The message to format.</param>
-    /// <returns>A formatted <see cref="LogMessage"/>.</returns>
-    protected virtual string Format(LogMessage message)
-    {
-        var builder = new StringBuilder();
-        builder.Append('[');
-        builder.Append(labels[message.Level]);
-        builder.Append(']');
-        builder.Append(' ');
-        builder.Append('[');
-        builder.Append(message.Timestamp.ToString("dd/MM/yyyy hh:mm:ss tt"));
-
-        if (message.Content is not null)
-        {
-            builder.Append(' ');
-            builder.Append(message.Content);
-        }
-
-        if (message.Exception is not null)
-        {
-            if (message.Content is not null)
-            {
-                builder.AppendLine();
-            }
-            else
-            {
-                builder.Append(' ');
-                builder.Append("An exception has occured.");
-            }
-
-            builder.Append(message.Exception);
-        }
-
-        builder.AppendLine();
-
-        return builder.ToString();
+        this.leaveOpen = leaveOpen;
     }
 
     ~LogWriterStream()
@@ -107,20 +38,15 @@ public class LogWriterStream : LogWriter, IDisposable
             return;
         }
 
-        stream.Dispose();
+        Flush();
+
+        if (!leaveOpen)
+        {
+            stream.Dispose();
+        }
 
         isDisposed = true;
 
         GC.SuppressFinalize(this);
     }
-
-    private static readonly Dictionary<LogLevel, string> labels = new()
-    {
-        { LogLevel.Trace,       "TRC" },
-        { LogLevel.Debug,       "DBG" },
-        { LogLevel.Information, "INF" },
-        { LogLevel.Warning,     "WRN" },
-        { LogLevel.Error,       "ERR" },
-        { LogLevel.Critical,    "CRT" },
-    };
 }
---
+++ source/Sekai.Framework/Logging/LogWriterText.cs
@@ -0,0 +1,68 @@
+// Copyright (c) Cosyne
+// Licensed under MIT. See LICENSE for details.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Sekai.Framework.Logging;
+
+/// <summary>
+/// A <see cref="LogWriter"/> that writes text.
+/// </summary>
+public class LogWriterText : LogWriterStream
+{
+    private readonly Stream stream;
+    private readonly Encoding encoding;
+
+    public LogWriterText(Stream stream, Encoding? encoding = null, bool leaveOpen = false)
+        : base(stream, leaveOpen)
+    {
+        this.stream = stream;
+        this.encoding = encoding ?? Encoding.UTF8;
+    }
+
+    public sealed override void Flush()
+    {
+        stream.Flush();
+    }
+
+    public sealed override void Write(LogMessage message)
+    {
+        string formatted = Format(message);
+
+        Span<byte> buffer = stackalloc byte[encoding.GetByteCount(formatted)];
+
+        if (encoding.GetBytes(formatted.AsSpan(), buffer) <= 0)
+        {
+            return;
+        }
+
+        stream.Write(buffer);
+    }
+
+    protected virtual string Format(LogMessage m) => string.Format(m.Exception is null ? format : format_with_exception, GetLevel(m), GetTimestamp(m), GetContent(m), GetException(m));
+    protected virtual string? GetLevel(LogMessage m) => designators[m.Level];
+    protected virtual string? GetContent(LogMessage m) => (m.Content is null && m.Exception is not null) ? format_exception_message : m.Content?.ToString();
+    protected virtual string? GetTimestamp(LogMessage m) => m.Timestamp.ToString(format_timestamp);
+    protected virtual string? GetException(LogMessage m) => m.Exception?.ToString();
+
+    private const string format_timestamp = "dd/MM/yyyy hh:mm:ss tt";
+    private const string format_exception_message = "An exception has occured.";
+    private const string format_header = "[{0}] [{1}]";
+    private const string format_content = "{2}";
+    private const string format_content_with_exception = "{2}\n{3}";
+    private const string format = $"{format_header} {format_content}\n";
+    private const string format_with_exception = $"{format_header} {format_content}\n{format_content_with_exception}\n";
+
+    private static readonly Dictionary<LogLevel, string> designators = new()
+    {
+        { LogLevel.Trace,       "TRC" },
+        { LogLevel.Debug,       "DBG" },
+        { LogLevel.Verbose, "INF" },
+        { LogLevel.Warning,     "WRN" },
+        { LogLevel.Error,       "ERR" },
+        { LogLevel.Critical,    "CRT" },
+    };
+}
