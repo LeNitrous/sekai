f6367a573740a6b2a9dd9b820853140bf51f5a27
Nathan Alo <nathan.alo2000@gmail.com>
2023-06-20 10:08:55 +0800

    make HotReload public

--- source/Sekai/HotReload.cs
+++ source/Sekai/HotReload.cs
@@ -5,14 +5,27 @@ using System;
 
 namespace Sekai;
 
-internal static class HotReloadCallbackReceiver
+/// <summary>
+/// A helper class that exposes methods related to .NET Hot Reload.
+/// </summary>
+public static class HotReload
 {
+    /// <summary>
+    /// Invoked when the metadata has updated.
+    /// </summary>
     public static event Action<Type[]?>? OnUpdate;
+
+    /// <summary>
+    /// Invoked when the cache has been cleared.
+    /// </summary>
+    public static event Action<Type[]?>? OnClear;
 
 #pragma warning disable IDE1006 // Required by MetadataUpdateHandler
 #pragma warning disable IDE0051 // Invoked via MetadataUpdateHandler
 
     private static void UpdateApplication(Type[]? updated) => OnUpdate?.Invoke(updated);
+
+    private static void ClearCache(Type[]? updated) => OnClear?.Invoke(updated);
 
 #pragma warning restore IDE1006
 #pragma warning restore IDE0051
